95da1baea8d42e7963171c1d395f71c7db2cf947
rpd-512 <rpd-512@users.noreply.github.com>
2025-10-18 17:19:34 +0530

    Debugging

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "include/nearest_neighbour_utils.h"
 #include "include/worker_utils.h"
 #include "include/refinement_utils.h"
+#include "include/refiner_content.h"
 #include <chrono>
 
 void clear_screen(){
@@ -29,7 +30,7 @@ int main(){
     dp.groundtruth = {11.0, 21.0, 31.0};
 
     NearestNeighbourEngine engine(3, {});
-    int n=10000;
+    int n=1000;
     vector<Datapoint> initial_points;
     for(int i=0; i<n; i++){
         initial_points.push_back(Datapoint{
@@ -63,5 +64,26 @@ int main(){
     duration = end - start;
     std::cout << "Linear search time: " << duration.count() << " microseconds" << std::endl;
 
+    cout << "\n\nTesting Refinement Engine with Gradient Descent Optimizer\n";
+
+
+    auto fwd_func = [](const Groundtruth& gt) {
+        cout << "Forward function called with groundtruth of size: " << gt.size() << endl;
+        Feature f = {1,2,3};
+        return f;
+    };
+    Feature target = {2,3,5};
+    GradientDescentOptimizer optimizer(fwd_func, LossFunction::cosine_error);
+    RefinementEngine ref_engine(&optimizer);
+    ref_engine.set_seed(dp);
+    ref_engine.set_logging(true);
+    ref_engine.set_target(target);
+    auto refined = ref_engine.refine(100);
+    cout << "Refined Datapoint:" << endl;
+    print_datapoint(Datapoint::from_vector(refined));
+    for(auto his: ref_engine.get_loss_history()) {
+        cout << his << ", ";
+    }
+    cout << endl;
     return 0;
 }
