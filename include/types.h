0d8f474b89a4ec0e43b2936c1fc7eae9f3577156
rpd-512 <rpd-512@users.noreply.github.com>
2025-10-19 06:39:35 +0530

    Updated utility

--- include/types.h
+++ include/types.h
@@ -11,13 +11,26 @@ using std::endl;
 using Feature = std::vector<double>;
 using Groundtruth = std::vector<double>;
 
-struct Datapoint {
+class Datapoint {
+public:
     int id;
-    Feature features;    // input features (position)
-    Groundtruth groundtruth; // value for that feature to exist (angle)Groundtruth
+    vector<double> features;     // input features (position)
+    vector<double> groundtruth;  // value for that feature to exist (angle)
+
+    // Constructors
+    Datapoint() : id(0), features({}), groundtruth({}) {}
+    Datapoint(int id_, const vector<double>& features_, const vector<double>& groundtruth_ = {})
+        : id(id_), features(features_), groundtruth(groundtruth_) {}
+
     // Static factory method
-    static Datapoint from_vector(const vector<double>& vec, int id = 0) {
-        return Datapoint{id, vec, {}};
+    static Datapoint from_vector(const vector<double>& ftr, const vector<double>& gth={}, int id = 0) {
+        return Datapoint{id, ftr, gth};
+    }
+
+    static vector<double> to_vector(const Datapoint& dp) {
+        vector<double> vec = dp.features;
+        vec.insert(vec.end(), dp.groundtruth.begin(), dp.groundtruth.end());
+        return vec;
     }
 };
 
